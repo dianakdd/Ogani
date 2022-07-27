def0d8955cc61ea8a25f9e969a611c248313cb66
Affan-97 <affana959@gmail.com>
2022-07-27 20:03:38 +0700

    Migration User

---
+++ database/migrations/2022_07_27_125158_create_profile_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('alamat',255);
+            $table->string('telepon',25);
+            $table->char('gender',1);
+            $table->date('tglLahir');
+            $table->string('foto',255);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
